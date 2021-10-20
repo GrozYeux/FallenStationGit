4d8cb030982127407ffb2b6500f9ad6b9792b264
GrozYeux <66424461+GrozYeux@users.noreply.github.com>
2021-10-19 23:03:47 -0400

    affichage en file

--- FallenStation/Assets/Scripts/UI/UIManager.cs
+++ FallenStation/Assets/Scripts/UI/UIManager.cs
@@ -6,11 +6,33 @@ using UnityEngine.UI;
 public class UIManager : AbstractSingleton<UIManager>
 {
     //le texte de feedback UI
-    [SerializeField] private Text affichageCollectable = null; 
+    [SerializeField] private Text affichageCollectable = null;
+
+    //pour savoir s'il y a déjà un texte
+    int line = 0;
+    List<string> allText = new List<string>();
+
     // Start is called before the first frame update
     void Start()
     {
         affichageCollectable.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        affichageCollectable.text = "";
+        if (allText.Count != 0)
+        {
+            affichageCollectable.gameObject.SetActive(true);
+            foreach (string s in allText)
+            {
+                affichageCollectable.text += s + "\n";
+            }
+        }
+        else
+        {
+            affichageCollectable.gameObject.SetActive(false);
+        }
     }
 
     //met à jour le texte à afficher
@@ -28,16 +50,21 @@ public class UIManager : AbstractSingleton<UIManager>
     //coroutine pour afficher petit à petit le texte
     IEnumerator PrintTextCorou(string text)
     {
-        affichageCollectable.gameObject.SetActive(true);
-        affichageCollectable.text = "";
+        int myline = line;
+        line += 1;
+        string affichage = "";
+        allText.Add(affichage);
         int i;
         string res = text + " collecté";
         for(i=0; i<res.Length; i++)
         {
-            affichageCollectable.text += res[i];
+            myline = allText.FindIndex((string s) => s == affichage);
+            affichage += res[i];
+            allText[myline] = affichage;
             yield return new WaitForSeconds(0.05f);
         }
         yield return new WaitForSeconds(3f);
-        affichageCollectable.gameObject.SetActive(false);
+        allText.Remove(res);
+        line -= 1;
     }
 }
