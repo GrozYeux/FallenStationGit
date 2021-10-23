2e97e6f83e6b4883b1b7979627a59eedabe51511
ClaraDel <clara.ferru@epfedu.fr>
2021-10-23 15:30:42 -0400

    gestion état chasse et attaque

--- FallenStation/Assets/Scripts/Enemy/EnemyRobotShooter.cs
+++ FallenStation/Assets/Scripts/Enemy/EnemyRobotShooter.cs
@@ -9,6 +9,9 @@ public class EnemyRobotShooter : EnemyBase
     private float lookRadius = 10f;
     Transform target;
     NavMeshAgent navMeshAgent;
+    public bool seesPlayer = false;
+    public float shootFrequency = 1.0f;
+    private float shootDelta = 0.0f;
 
     protected override void Start()
     {
@@ -20,15 +23,17 @@ public class EnemyRobotShooter : EnemyBase
     protected override void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
-        if (distance <= lookRadius)
+        if (distance <= lookRadius && distance >= navMeshAgent.stoppingDistance)
         {
-            navMeshAgent.SetDestination(target.position);
+            seesPlayer = true;
+            ChaseState(); //a enlever plus tard
 
-            if (distance <= navMeshAgent.stoppingDistance)
-            {
-                //attack the target
-                faceTarget();
-            }
+        } else if (distance <= navMeshAgent.stoppingDistance) {
+            //attack the target
+            AttackState();//a enlever plus tard
+
+        } else {
+            seesPlayer = false;
         }
     }
 
@@ -38,6 +43,53 @@ public class EnemyRobotShooter : EnemyBase
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
 
+    }
+    protected override void IdleState()
+    {
+        if (seesPlayer)
+        {
+            //currentState = State.Attack;
+        }
+    }
+
+    protected override void ChaseState()
+    {
+        navMeshAgent.SetDestination(target.position);
+        faceTarget();
+    }
+
+    protected override void AttackState()
+    {
+        Debug.Log("AttackState");
+
+        //Check the player visibility
+        if (seesPlayer == false)
+        {
+            currentState = State.Idle;
+            return;
+        }
+
+        faceTarget();
+
+        //Shoot the player, accounting the frequency
+        if (shootDelta > shootFrequency)
+        {
+            shootDelta = 0.0f;
+            Hit();
+        }
+        else
+        {
+            shootDelta += Time.deltaTime;
+        }
+    }
+
+    protected override void Hit()
+    {
+        /*GameObject bullet = (GameObject)Instantiate(projectile, shootPoint.transform.position, shootPoint.transform.rotation);
+        bullet.SetActive(true);
+        Physics.IgnoreCollision(bullet.GetComponent<Collider>(), body.GetComponent<Collider>());
+        bullet.GetComponent<Rigidbody>().AddForce(shootPoint.transform.forward * 1200, ForceMode.Acceleration);*/
+        Debug.Log("Hit");
     }
 
     private void OnDrawGizmosSelected()
