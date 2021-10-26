5b2624405bce5be5107d901a76d36ab11ed4e047
IlonaPw <74210552+IlonaPw@users.noreply.github.com>
2021-10-26 18:05:22 -0400

    Codex sur MenuPause

--- FallenStation/Assets/Scripts/Menu/CodexManager.cs
+++ FallenStation/Assets/Scripts/Menu/CodexManager.cs
@@ -8,10 +8,19 @@ public class CodexManager : MonoBehaviour
     public GameObject content;
     private Button button;
     private Button[] buttons;
+
+    
+    public GameObject panel;
+    public GameObject Menu;
+    GameObject CanvasCodex;
+    GameObject EventSystem;
+    public GameObject ButtonCodex;
     // Start is called before the first frame update
     void Start()
     {
         buttons = content.GetComponentsInChildren<Button>();
+        CanvasCodex = GameObject.Find("Codex");
+        EventSystem = GameObject.Find("EventSystem");
 
     }
 
@@ -22,5 +31,14 @@ public class CodexManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha2)) buttons[1].interactable = true;
         if (Input.GetKeyDown(KeyCode.Alpha3)) buttons[2].interactable = true;
         if (Input.GetKeyDown(KeyCode.Alpha4)) buttons[3].interactable = true;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CanvasCodex.SetActive(false);
+            Menu.SetActive(true);
+            panel.SetActive(true);
+
+            EventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(ButtonCodex);
+        }
     }
 }
