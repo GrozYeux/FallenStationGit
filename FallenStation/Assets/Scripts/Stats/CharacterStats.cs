93b01acfdabb27b09f5892f71622d2d8fa3b142d
ClaraDel <clara.ferru@epfedu.fr>
2021-11-01 20:17:17 -0400

    Dernières fonctionnalités du sprint 2

--- FallenStation/Assets/Scripts/Stats/CharacterStats.cs
+++ FallenStation/Assets/Scripts/Stats/CharacterStats.cs
@@ -6,22 +6,14 @@ public class CharacterStats : MonoBehaviour
 {
     public float damage = 20 ;
     public float maxHealth = 100;
-    public float currentHealth { get; private set; }
-    void Start()
-    {
-      
-    }
-    void Awake()
-    {
-        currentHealth = maxHealth;
-    }
+    public float currentHealth { get; protected set; }
+
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         Debug.Log(transform.name + " takes " + damage + " damage. He has " + currentHealth + " of currentHealth.");
         Hurt((maxHealth - currentHealth) / maxHealth);
-        
 
         if (currentHealth <= 0)
         {
@@ -32,12 +24,19 @@ public class CharacterStats : MonoBehaviour
     protected virtual void Die()
     {
         //die in some way
-        // to be overwritten
+        //overwritten
+
     }
     protected virtual void Hurt(float Alpha)
     {
         //had been hurting
-        // to be overwritten
+        // overwritten
+    }
+
+    protected virtual void Awake()
+    {
+        //overwritten
+
     }
 
 }
--- FallenStation/Assets/Scripts/Stats/PlayerStats.cs
+++ FallenStation/Assets/Scripts/Stats/PlayerStats.cs
@@ -6,24 +6,63 @@ using UnityEngine.UI;
 public class PlayerStats : CharacterStats
 {
     GameObject damagePanel ;
+    private float regenDelta = 0.0f;
+    public float regenFrequency = 4.0f;
+
     private void Start()
     {
         damagePanel = GameObject.Find("Canvas/DamagePanel");
-        damagePanel.SetActive(false);
-
+        modifyDamagePanel(0.0f);
+    }
+    private void Update()
+    {
+        if (currentHealth < maxHealth)
+        {
+            Regen();
+        }
     }
     protected override void Die()
     {
-        //damagePanel.SetActive(false);
         base.Die();
         Debug.Log(transform.name + " died.");
         //reload the scene 
+        //Regeneration
+        Awake();
     }
 
-    protected override void Hurt(float newAlpha)
+    protected override void Hurt(float newAlpha )
     {
+        modifyDamagePanel(newAlpha);
+    }
 
-        damagePanel.SetActive(true);
+    private void Regen()
+    {
+        //regenerate the player, accounting the frequency
+        if (regenDelta > regenFrequency)
+        {
+            currentHealth += 10;
+
+            modifyDamagePanel((maxHealth - currentHealth) / maxHealth);
+
+            regenDelta = 0.0f;
+        }
+        else
+        {
+            regenDelta += Time.deltaTime;
+        } 
+
+    }
+    protected override void Awake()
+    {
+        currentHealth = maxHealth;
+        modifyDamagePanel(0.0f);
+    }
+
+    protected void modifyDamagePanel(float newAlpha)
+    {
+        //damagePanel = GameObject.Find("Canvas/DamagePanel");
+        //damagePanel.SetActive(true);
+        damagePanel = GameObject.Find("Canvas/DamagePanel");
         Image image = damagePanel.GetComponent<Image>();
         var tempColor = image.color;
         tempColor.a = newAlpha;
