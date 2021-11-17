4b75d5101f782db758974847ea40cb2b0994c2c6
IlonaPw <74210552+IlonaPw@users.noreply.github.com>
2021-11-17 12:22:22 -0500

    Spore

---
+++ FallenStation/Assets/Scripts/Enemy/Mushroom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+    public class Mushroom : MonoBehaviour
+    {
+        [SerializeField]  private GameObject spore;
+        private GameObject player;
+
+        private void Start()
+        {
+            player = GameManager.Instance.GetPlayer();
+            spore.SetActive(false);
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            StartCoroutine(Spore());
+
+         }
+        IEnumerator Spore()
+        {
+            spore.SetActive(true);
+            yield return new WaitForSeconds(3f);
+            spore.SetActive(false);
+    }
+    }
